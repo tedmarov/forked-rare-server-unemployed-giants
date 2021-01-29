bcd599a5a1bb411a9b66ca71206f896fc1b6b60e
ted <tedmarov@gmail.com>
2021-01-29 05:56:32 -0600

    Tag View, Create, Delete works

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from .users import User
 from .post import Post
 from .comment import Comment
 from .category import Category
+from .tag import Tag
---
+++ models/tag.py
@@ -0,0 +1,8 @@
+class Tag():
+
+    # Class initializer. It has 2 custom parameters, with the
+    # special `self` parameter that every method on a class
+    # needs as the first parameter.
+    def __init__(self, id, label):
+        self.id = id
+        self.label = label
